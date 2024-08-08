641a7085751a06f87e7ba53de32594c32f8236cf
DezertIgor <wasuta.work@gmail.com>
2024-08-08 15:57:52 +0300

    Практикум 8

---
+++ Автоматизация тестирования/05. PageObject/1__test_form.py
@@ -0,0 +1,13 @@
+from PageObject import Form
+
+
+def test():
+    form = Form()
+    form.get()
+    form.filling()
+    form.click()
+    must_be_red = Form.color_of_red_element()
+    list_of_colors = Form.color_of_green_elements()
+    assert must_be_red == 'rgba(248, 215, 218, 1)'
+    for color in list_of_colors:
+        assert color == "rgba(209, 231, 221, 1)"
